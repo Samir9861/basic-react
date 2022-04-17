324636bf254a552bfa7bf9c9a3f6465eb849cce4
Samir9861 <samirkarki9861@gmail.com>
2022-04-17 20:11:39 +0545

    basic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Button from "./Button";
+const App = () => {
+  return (
+    <div>
+      <h1>😊😊😊😊WELCOME😊😊😊😊</h1>
+      <h3>which website do you want to open</h3>
+      <h5>just click one an only</h5>
+      <Button name="Google" link="https://google.com" />
+      <Button name="Facebook" link="https://www.facebook.com" />
+      <Button name="Youtube" link="https://www.youtube.com/" />
+      <Button name="ipo result" link="https://iporesult.cdsc.com.np" />
+      <Button name="github" link="https://github.com/" />
+    </div>
+  );
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import App from "./App";
+import Button from "./Button";
+
+ReactDOM.render(<App />, document.getElementById("component"));
